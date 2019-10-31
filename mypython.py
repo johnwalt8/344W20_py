53468fdd2938234104560336b52fcdc9dc1a1eb5
walter_johnson <walter_johnson@trimble.com>
2019-10-31 16:43:21 -0700

    I think this is it

---
+++ mypython.py
@@ -0,0 +1,13 @@
+import string
+import random
+fileLetters = ['A', 'B', 'C']
+myfile = 'myfile'
+for i in range(3):
+    myfile = 'myfile' + fileLetters[i]
+    file= open(myfile, "w+")
+    randomString = ''
+    for i in range(10):
+        randomString += random.choice(string.ascii_lowercase)
+    randomString += '\n'
+    file.write(randomString)
+    file.close()
