a00bd58780f61366634ded1951784a32e1d4af0f
Walter Johnson <johnwalt@oregonstate.edu>
2020-02-06 11:08:20 -0800

    new version with functions and structure to make it more self-explanatory

--- mypython.py
+++ mypython.py
@@ -1,25 +1,53 @@
 # mypython.py
 # Walter Johnson
-# Program Py: CS344 Operating Systems I, Fall 2019
+# Program Py: CS344 Operating Systems I, Winter 2020
 
 import string
 import random
 import sys
-random.seed()
-fileLetters = ['A', 'B', 'C']
-myfile = 'myfile'
-for i in range(3):
-    myfile = 'myfile' + fileLetters[i]
-    file= open(myfile, "w+")
+
+# takes a quantity, a string for the file name, and more strings that will each be appended to the file name; returns a list of the names of the files created
+def createFilesContainingRandomString(stringLength, fileName, *fileNameSuffixes):
+    fileNameList = []
+    for fileNameSuffix in fileNameSuffixes: # for each of the strings to be appended
+        myfile = fileName + fileNameSuffix
+        myString = createRandomString(stringLength)
+        file = open(myfile, "w+")
+        file.write(myString)
+        file.close()
+        fileNameList.append(myfile)
+    return fileNameList
+
+# takes a number for the length of the string to be created, returns the string
+def createRandomString(numElements):
     randomString = ''
-    for j in range(10):
+    for i in range(numElements): 
         randomString += random.choice(string.ascii_lowercase)
     randomString += '\n'
-    file.write(randomString)
-    sys.stdout.write(randomString)
-    file.close()
-randomIntA = random.randint(1, 42)
-print(randomIntA)
-randomIntB = random.randint(1, 42)
-print(randomIntB)
-print(randomIntA * randomIntB)
+    return randomString
+
+# takes a list of files names, writes entire contents of each file to standard output
+def readAndPrintFileContents(fileNameList):
+    for fileName in fileNameList:
+        file = open(fileName, "r")
+        fileContents = file.read()
+        sys.stdout.write(fileContents)
+        file.close()
+
+# takes a pair of integers representing minimum and maximum value, returns integer
+def createAndPrintRandomInt(min, max):
+    randomInt = random.randint(min, max)
+    print(randomInt)
+    return randomInt
+
+def main():
+    random.seed()
+
+    listOfFileNames = createFilesContainingRandomString(10, 'myfile', 'A', 'B', 'C')
+
+    readAndPrintFileContents(listOfFileNames)
+
+    # after random ints are printed, multiplies those ints and prints product
+    print(createAndPrintRandomInt(1, 42) * createAndPrintRandomInt(1, 42))
+
+main()
---
+++ mypythonF19.py
@@ -0,0 +1,25 @@
+# mypython.py
+# Walter Johnson
+# Program Py: CS344 Operating Systems I, Fall 2019
+
+import string
+import random
+import sys
+random.seed()
+fileLetters = ['A', 'B', 'C']
+myfile = 'myfile'
+for i in range(3):
+    myfile = 'myfile' + fileLetters[i]
+    file= open(myfile, "w+")
+    randomString = ''
+    for j in range(10):
+        randomString += random.choice(string.ascii_lowercase)
+    randomString += '\n'
+    file.write(randomString)
+    sys.stdout.write(randomString)
+    file.close()
+randomIntA = random.randint(1, 42)
+print(randomIntA)
+randomIntB = random.randint(1, 42)
+print(randomIntB)
+print(randomIntA * randomIntB)
